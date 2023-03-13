34cc98521cd0656eef42ad9f7f6176ff2b32d17b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-13 16:41:45 +0000

    echoing client secret length

--- api/GetStatsTrigger/index.js
+++ api/GetStatsTrigger/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const responseMessage = "The stats will appear here";
+    const responseMessage = "The stats will appear here - " + process.env.CLIENT_ID;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
