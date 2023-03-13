178d1e52adbb3d4184e99ccb712779a228594adb
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-13 16:48:36 +0000

    Preparing to not check in token!

--- api/GetStatsTrigger/index.js
+++ api/GetStatsTrigger/index.js
@@ -1,7 +1,9 @@
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const responseMessage = "The stats will appear here - " + process.env.CLIENT_ID;
+    const responseMessage = "The stats will appear here - " + process.env.PROJECT_ID;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
