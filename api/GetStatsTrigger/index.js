b16a500210ae1dc39c7bcf6be0983fe7da719bfd
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-14 12:00:29 +0000

    Adding values

--- api/GetStatsTrigger/index.js
+++ api/GetStatsTrigger/index.js
@@ -47,7 +47,7 @@ module.exports = async function (context, req) {
     
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: res
+            body: res.data.values
         };
     } catch (err) {
         context.log(err);
