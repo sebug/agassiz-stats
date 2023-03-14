145e6679985f51655f64d8b773706521ae660021
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-14 10:31:54 +0000

    Adding full content again

--- api/GetStatsTrigger/index.js
+++ api/GetStatsTrigger/index.js
@@ -4,12 +4,33 @@ const process = require('process');
 const {google} = require('googleapis');
 const {authenticate} = require('@google-cloud/local-auth');
 
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize(context) {
+    context.log('Service account key length: ' + process.env.SERVICE_ACCOUNT_KEY.length)
+
+    await fs.writeFile("keyfile.json", process.env.SERVICE_ACCOUNT_KEY);
+
+    client = await authenticate({
+      scopes: SCOPES,
+      keyfilePath: "keyfile.json",
+    });
+    return client;
+  }
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('Begin get stats.');
 
-    context.log(authenticate);
+    const client = await authorize(context);
 
-    const responseMessage = "Hello from getting stats";
+    context.log(client);
+
+    const responseMessage = "The stats will appear here - " + process.env.PROJECT_ID;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
